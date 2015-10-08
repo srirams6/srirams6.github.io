348440d432f2069ab081fdd1c679b540c6b62d79
Sriram Sundarraj <sriram.s.1994@gmail.com>
2015-10-08 22:34:17 +0530

    [Misc] fixed scroll and added key support - ipproj

--- misc/ipproj/script.js
+++ misc/ipproj/script.js
@@ -2,20 +2,39 @@ var sdegree = 0;
 var mleft = 10;
 $(window).bind('mousewheel', function(event) {
     if (event.originalEvent.wheelDelta >= 0) {
-        // console.log('Scroll up');
-	    sdegree ++ ;
-	    sdegree = sdegree + 3 ;
-	    mleft = mleft + 3;
-    }
-    else {
         // console.log('Scroll down');
 	    sdegree -- ;
 	    sdegree = sdegree - 3 ;
 	    mleft = mleft - 3;
+    }
+    else {
+        // console.log('Scroll up');
+	    sdegree ++ ;
+	    sdegree = sdegree + 3 ;
+	    mleft = mleft + 3;
     }
 	var srotate = "rotate(" + sdegree + "deg)";
 	var mleftCSS = "" + mleft + "px"; 
     $(".wheel").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
     $(".wheel").css({"margin-left" : mleftCSS});
     return false;
+});
+$(window).keydown(function(event){
+	console.log(event.which);
+	if (event.which == 40 | event.which ==39) {
+		// console.log('Scroll up');
+	    sdegree ++ ;
+	    sdegree = sdegree + 3 ;
+	    mleft = mleft + 3;
+	}
+	else if(event.which == 38 | event.which == 37){
+		// console.log('Scroll down');
+	    sdegree -- ;
+	    sdegree = sdegree - 3 ;
+	    mleft = mleft - 3;
+	}
+	var srotate = "rotate(" + sdegree + "deg)";
+	var mleftCSS = "" + mleft + "px"; 
+    $(".wheel").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+    $(".wheel").css({"margin-left" : mleftCSS});
 });
